638978f7017577a3eac6c90f7fb29a11f4b96609
mhouacine <mhouacine@octo.com>
2021-12-01 16:03:17 +0100

    day 1 - part 2 ✔

--- day_1/__main__.py
+++ day_1/__main__.py
@@ -1,16 +1,29 @@
-from typing import List
+from typing import List, Tuple
 
 
-def number_times_depth_increases(depths: List[int]):
-    return sum((1 if (pair[0] < pair[1]) else 0 for pair in (_list_to_pairs(depths))))
+def number_times_depth_increases(depths: List[int], measurement_sliding_window: int = 1):
+    groups_of_measurements: List[Tuple[int]] = group_measurements(depths, measurement_sliding_window)
+    sums_by_group: List[int] = sum_groups_of_measurements(groups_of_measurements)
+    tuples_of_depths = _list_to_tuples_of_depths(sums_by_group)
+    return sum((1 if (pair[0] < pair[1]) else 0 for pair in tuples_of_depths))
 
 
-def _list_to_pairs(depths):
+def _list_to_tuples_of_depths(depths: List[int]):
     for i in range(0, len(depths) - 1):
         yield tuple(depths[i:i + 2])
+
+
+def group_measurements(depths: List[int], measurement_sliding_window: int) -> List[Tuple[int]]:
+    for i in range(0, len(depths) - measurement_sliding_window + 1):
+        yield tuple(depths[i:i + measurement_sliding_window])
+
+
+def sum_groups_of_measurements(groups) -> List[int]:
+    return [sum(group) for group in groups]
 
 
 if __name__ == '__main__':
     with open('input.txt') as f:
         depths: List[int] = [int(n) for n in f.readlines()]
     print(number_times_depth_increases(depths))  # 1752
+    print(number_times_depth_increases(depths, measurement_sliding_window=3))  # 1781
---
+++ tests/test_day_1.py
@@ -0,0 +1,77 @@
+from day_1.__main__ import number_times_depth_increases, group_measurements, sum_groups_of_measurements
+
+
+def test_no_increase():
+    # Given
+    depths = [199, 199]
+
+    # When
+    increases = number_times_depth_increases(depths)
+
+    # Then
+    assert increases == 0
+
+
+def test_one_increase():
+    # Given
+    depths = [199, 200]
+
+    # When
+    increases = number_times_depth_increases(depths)
+
+    # Then
+    assert increases == 1
+
+
+def test_seven_increases():
+    # Given
+    depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+
+    # When
+    increases = number_times_depth_increases(depths)
+
+    # Then
+    assert increases == 7
+
+
+#### Part 2
+
+def test_group_measurements_with_sliding_window_of_3():
+    # Given
+    depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+
+    # When
+    groups = group_measurements(depths, 3)
+
+    # Then
+    assert list(groups) == [
+        (199, 200, 208), (200, 208, 210), (208, 210, 200),
+        (210, 200, 207), (200, 207, 240), (207, 240, 269),
+        (240, 269, 260), (269, 260, 263)
+    ]
+
+
+def test_sum_of_groups_of_measurements():
+    # Given
+    groups = [
+        (199, 200, 208), (200, 208, 210), (208, 210, 200),
+        (210, 200, 207), (200, 207, 240), (207, 240, 269),
+        (240, 269, 260), (269, 260, 263)
+    ]
+
+    # When
+    sums = sum_groups_of_measurements(groups)
+
+    # Then
+    assert sums == [607, 618, 618, 617, 647, 716, 769, 792]
+
+
+def test_depths_increase_on_day_2():
+    # Given
+    depths = [607, 618, 618, 617, 647, 716, 769, 792]
+
+    # When
+    increases = number_times_depth_increases(depths)
+
+    # Then
+    assert increases == 5
